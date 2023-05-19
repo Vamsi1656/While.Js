57b04c7b78d6cd5f4847f2dbafdbfdf30e8b7698
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-19 12:55:48 +0530

    first push

--- one.js
+++ one.js
@@ -10,3 +10,31 @@ while(i>=1){
     console.log(i)
     i--
 }*/
+/*write a program to print 1 to 10 even  numbers
+i=0
+while(i<20){
+    console.log(i)
+    i=i+2
+*/
+/*write a program to print multiples of 4
+i=4
+while(i<=40){
+    console.log(i)
+    i=i+4
+}*/
+/*write a program to print mutliplication table of 6
+i=1
+while(i<=10){
+    console.log("6*",i,"=",6*i)
+    i++
+}*/
+/*factors of 24
+i=1
+while(i<=24){
+    if(24%i===0)
+       console.log(i)
+        i++
+      }*/
+      const num = 12345;
+      const reverse = parseInt(num.toString().split('').reverse().join(''));
+      console.log(reverse);
