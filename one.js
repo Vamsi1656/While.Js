e2e5dbc6fb9a926bcf37082b60454b29bbe3d360
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-19 12:00:11 +0530

    First commit

---
+++ one.js
@@ -0,0 +1,12 @@
+/*write a code to print 1 to 10 numbers
+let i=1
+while(i<=10){
+    console.log(i)
+    i++
+}*/
+/*write a code to print 1 to 10 numbers
+i=10
+while(i>=1){
+    console.log(i)
+    i--
+}*/
